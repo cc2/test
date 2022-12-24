506ed17f5fdd4bb9f0b12e71f01a713f63b13132
kamyar <5577162+cc2@users.noreply.github.com>
2022-12-24 03:53:40 -0800

    aaaa

---
+++ app/utilities.py
@@ -0,0 +1,8 @@
+# i want to access variables in config here
+class Utilities:
+    def __init__(self) -> None:
+        pass
+
+    def test(self):
+        print("from Utilities")
+        print(configs.client_id)
--- config.py
+++ config.py
@@ -1,17 +1,11 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-
+# i want to use a class for all the global variables
 class Configs:
-    def __init__(self, is_prod):
-        """False -> Dev , True -> Production"""
-        self.__load_variables(is_prod)
+    def __init__(self, is_prod) -> None:
+        self.init(is_prod)
 
-    def __load_variables(self, is_prod):
+    def init(self, is_prod):
         self.is_prod = is_prod
-
-        self.podio_client_id = 100
-        self.podio_client_secret = 200
-
-        logger.error(f"Successfully loaded env variables for {'PROD' if self.is_prod else 'DEV'} environment")
+        if self.is_prod:
+            self.client_id = 100
+        else:
+            self.client_id = 300
